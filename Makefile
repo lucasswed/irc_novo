b3aeac072d8ed516eba7be3ba3676cd70eab0384
lucas wsl <lucas@Lucas-Desktop>
2023-11-15 19:50:48 +0000

    first commit

---
+++ Makefile
@@ -0,0 +1,73 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: lucas-ma <lucas-ma@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/11/15 19:08:33 by lucas-ma          #+#    #+#              #
+#    Updated: 2023/11/15 19:08:41 by lucas-ma         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+################ PROGRAM ################
+
+NAME	=	 ircserv
+
+################ TERMINAL ###############
+
+RMV		=	rm -f
+MKD		=	mkdir
+PRT		=	printf
+MKE		=	make
+CPY		=	cp
+
+################# COLORS ################
+
+--GRN	=	\033[32m
+--RED	=	\033[31m
+--WHT	=	\033[39m
+
+################# DIRS ##################
+
+_SRC	=	src/
+_OBJ	=	obj/
+_LIB	=	libs/
+_BIN	=	./
+
+############### COMPILER ################
+
+CC		=	c++
+CFLAGS	=	-Wall -Werror -Wextra -std=c++98 -g -fsanitize=address
+#valgrind --leak-check=full --show-leak-kinds=all ./
+SRCS	=	$(_SRC)main.cpp
+OBJS	=	$(patsubst $(_SRC)%.cpp,$(_OBJ)%.o,$(SRCS))
+DEPS	=
+LIBS	=
+
+################ RULES ##################
+
+all: $(NAME)
+
+$(_OBJ)%.o: $(_SRC)%.cpp
+	$(MKD) -p $(@D)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(NAME): $(DEPS) $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) -o $(NAME)
+
+################ DEPS ###################
+
+############## STRUCTURE ################
+
+clean:
+	$(RMV) -r $(_OBJ)
+
+fclean: clean
+	$(RMV) -r $(NAME)
+	
+re: fclean all
+
+rebonus: fclean bonus
+
+.PHONY: all deps clean fclean re
---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucas-ma <lucas-ma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 19:22:33 by lucas-ma          #+#    #+#             */
+/*   Updated: 2023/11/15 19:50:31 by lucas-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "irc.hpp"
+
+bool strIsNumber(std::string const& str) {
+	size_t i = 0;
+
+	if (str[0] == '-' || str[0] == '+')
+		i++;
+	for (; str[i]; i++) {
+		if (!std::isdigit(str[i]))
+			return (false);
+	}
+	return (true);
+}
+
+bool check_errors(char **av) {
+	if (!strIsNumber(av[1])) {
+		std::cerr << "Port argument is not a number!" << std::endl;
+		return (false);
+	}
+
+	int port = std::atoi(av[1]);
+	if (port < 0 || port > MAX_PORT) {
+		//IPPORT_USERRESERVED
+		std::cerr << "Port outside of range!" << std::endl;
+		std::cerr << "The range is [0 , " << MAX_PORT << "]" << std::endl;
+		return (false);
+	}
+
+	std::string teste(av[2]);
+	if (teste.empty()) {
+		std::cerr << "Empty password!" << std::endl;
+		return (false);
+	}
+
+	for (size_t i = 0; teste[i]; i++) {
+		if (std::isspace(teste[i])) {
+			std::cerr << "The password must not contain spaces!" << std::endl;
+			return (false);
+		}
+	}
+	
+	return (true);
+}
+
+int main(int ac, char **av) {
+	if (ac != 3) {
+		std::cerr << "Wrong arguments number!" << std::endl;
+		std::cerr << "Right usage: ./ircserv <port> <password>" << std::endl;
+		return (1);
+	}
+
+	if (!check_errors(av))
+		return (1);
+}
